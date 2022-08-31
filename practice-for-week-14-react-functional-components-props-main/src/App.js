893057d19c3798a40e0a8f086f6e55f572fc351b
Daniel Yoo <dacapo0509@gmail.com>
2022-08-30 21:04:10 -0700

    props practice complete

--- practice-for-week-14-react-functional-components-props-main/src/App.js
+++ practice-for-week-14-react-functional-components-props-main/src/App.js
@@ -1,12 +1,27 @@
-import Showcase from './Showcase';
-import './App.css';
+import Showcase from "./Showcase";
+import BaseStats from "./BaseStats";
+import "./App.css";
 
 function App() {
-  return (
-    <div className='main-wrapper background'>
-      <Showcase />
-    </div>
-  );
+	const baseStats = {
+		hp: 45,
+		attack: 49,
+		defense: 49,
+		spAttack: 65,
+		spDef: 65,
+		speed: 45,
+	};
+	const handleClick = () => {
+		alert(
+			`Special Stats\n\tSpecial Attack: ${baseStats.spAttack}\n\tSpecial Defense: ${baseStats.spDef}`
+		);
+	};
+	return (
+		<div className="main-wrapper background">
+			<Showcase />
+			<BaseStats clicker={handleClick} stats={baseStats} />
+		</div>
+	);
 }
 
 export default App;
